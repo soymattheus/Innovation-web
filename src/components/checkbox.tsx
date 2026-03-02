7457f99d553ece565cd7dab978bd7cfa5276ad5b
Matheus Tavares <matheustavares1a@gmail.com>
2026-03-02 16:08:55 -0300

    fix/svg icon

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,3 +1,5 @@
+import { CheckIcon } from "lucide-react";
+
 export default function Checkbox({
   checked,
   onChange,
@@ -21,22 +23,7 @@ export default function Checkbox({
         className="flex flex-row items-center gap-2 cursor-pointer"
       >
         <div className="w-4 h-4 border border-white rounded-sm">
-          {checked && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="15"
-              height="15"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-check-icon lucide-check"
-            >
-              <path d="M20 6 9 17l-5-5" />
-            </svg>
-          )}
+          {checked && <CheckIcon className="w-3.75 h-3.75 text-white" />}
         </div>
         {label}
       </label>
